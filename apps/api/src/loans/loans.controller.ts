c914a3f4847f8c40e3ab83706bf9025827b32973
Isaias Rojas <s_isaiasr@tutanota.com>
2023-08-24 14:04:34 -0400

    feat: add format response for loans

--- apps/api/src/loans/loans.controller.ts
+++ apps/api/src/loans/loans.controller.ts
@@ -5,37 +5,62 @@ import {
   Get,
   Param,
   Patch,
-  Post
+  Post,
+  UseGuards
 } from '@nestjs/common';
+import {
+  ApiBearerAuth,
+  ApiCreatedResponse,
+  ApiOkResponse,
+  ApiTags
+} from '@nestjs/swagger';
+import JwtAuthGuard from 'src/auth/jwt-auth.guard';
 import CreateLoanDto from './dto/create-loan.dto';
 import UpdateLoanDto from './dto/update-loan.dto';
+import LoanEntity from './entities/loan.entity';
 import LoansService from './loans.service';
 
+@ApiTags('loans')
 @Controller('loans')
 export default class LoansController {
   constructor(private readonly loansService: LoansService) {}
 
   @Post()
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
+  @ApiCreatedResponse({ type: CreateLoanDto })
   create(@Body() clientId: number, createLoanDto: CreateLoanDto) {
     return this.loansService.create(clientId, createLoanDto);
   }
 
   @Get()
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
+  @ApiOkResponse({ type: LoanEntity, isArray: true })
   findAll() {
     return this.loansService.findAll();
   }
 
   @Get(':id')
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
+  @ApiOkResponse({ type: LoanEntity })
   findOne(@Param('id') id: string) {
     return this.loansService.findOne(+id);
   }
 
   @Patch(':id')
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
+  @ApiOkResponse({ type: LoanEntity })
   update(@Param('id') id: string, @Body() updateLoanDto: UpdateLoanDto) {
     return this.loansService.update(+id, updateLoanDto);
   }
 
   @Delete(':id')
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
+  @ApiOkResponse({ type: LoanEntity })
   remove(@Param('id') id: string) {
     return this.loansService.remove(+id);
   }
