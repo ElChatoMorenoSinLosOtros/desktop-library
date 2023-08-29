05dfb2e24dddea602c9a0387c40b778adb6bac18
Hairax <hairaxelpoderoso@gmail.com>
2023-08-28 21:38:08 -0400

    add insert in DB

--- apps/db/sql/query.sql
+++ apps/db/sql/query.sql
@@ -14,3 +14,14 @@ INSERT INTO "user" ("name", "lastName", "address", "email", "phoneNumber", "type
 ('Andres', 'Ortega', 'Calle 13', 'andres.ortega@gmail.com', 1234579, 'Student'),
 ('Camila', 'Cruz', 'Calle 14', 'camila.cruz@gmail.com', 1234580, 'Student'),
 ('Javier', 'Jimenez', 'Calle 15', 'javier.jimenez@gmail.com', 1234581, 'Student');
+
+INSERT INTO "Materials" ("title", "author", "category", "isbn", "publicationYear", "pageCount", "quantity", "available", "type_material") VALUES
+('El Gran Gatsby', 'F. Scott Fitzgerald', 'Novela', '978-0141182636', 1925, 218, 20, 20, 'BOOK'),
+('Crimen y Castigo', 'Fiodor Dostoievski', 'Novela', '978-8420609064', 1866, 672, 15, 15, 'BOOK'),
+('Los Juegos del Hambre', 'Suzanne Collins', 'Ciencia Ficción', '978-8427202135', 2008, 396, 12, 12, 'BOOK'),
+('Orgullo y Prejuicio', 'Jane Austen', 'Romance', '978-8491050390', 1813, 416, 18, 18, 'BOOK'),
+('El Hobbit', 'J.R.R. Tolkien', 'Fantasía', '978-8445071659', 1937, 320, 25, 25, 'BOOK'),
+('National Geographic', 'Varios autores', 'Ciencia y Naturaleza', '555-1234', 2023, 64, 30, 30, 'REVISTA'),
+('Time Magazine', 'Varios autores', 'Actualidad', '123-5678', 2023, 48, 25, 25, 'REVISTA'),
+('Scientific American', 'Varios autores', 'Ciencia', '123-4567', 2023, 72, 40, 40, 'REVISTA'),
+('National Geographic Traveler', 'Varios autores', 'Viajes', '555-5678', 2023, 56, 20, 20, 'REVISTA');
