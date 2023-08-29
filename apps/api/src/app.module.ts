040487b1c12eb45ec1c26841afcddea3c789a876
José Luis Terán <joseluisteran2617@gmail.com>
2023-08-29 12:04:48 -0400

    feat: update module import

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
-import { ReservationsModule } from './reservations/reservations.module';
+import PrismaModule from './prisma/prisma.module';
 import AdminsModule from './admins/admins.module';
 import AppController from './app.controller';
 import AppService from './app.service';
 import AuthModule from './auth/auth.module';
 import ClientsModule from './clients/clients.module';
 import MaterialsModule from './materials/materials.module';
-import PrismaModule from './prisma/prisma.module';
+import ReservationsModule from './reservations/reservations.module';
 
 @Module({
   imports: [
