e4355b7371ed0e9714a9c47cc5c095b6ea5fd7ed
Isaias Rojas <s_isaiasr@tutanota.com>
2023-09-02 17:21:51 -0400

    feat: rest quantity when making a loan

--- apps/api/src/loans/loans.service.ts
+++ apps/api/src/loans/loans.service.ts
@@ -22,17 +22,25 @@ export default class LoansService {
       where: { materialId }
     });
 
-    if (!material || material.available) {
+    if (!material || material.quantity === 0) {
       throw new Error('Material not available');
-    } else if (material.quantity === 1) {
-      await this.prisma.material.update({
-        where: { materialId },
-        data: { quantity: 0, available: false }
-      });
     }
+
     const createdLoan = await this.prisma.loan.create({
       data: loanData
     });
+
+    const newQuantity = material.quantity - 1;
+    const materialUpdateData = {
+      quantity: newQuantity,
+      available: newQuantity > 0 ? material.available : false
+    };
+
+    await this.prisma.material.update({
+      where: { materialId },
+      data: materialUpdateData
+    });
+
     return createdLoan;
   }
 
