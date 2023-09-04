3794f97752d0d14f37b6e8c671d7a45ef451c804
Isaias Rojas <s_isaiasr@tutanota.com>
2023-09-04 11:08:25 -0400

    fix: test run

--- apps/api/test/offices/offices.controller.spec.ts
+++ apps/api/test/offices/offices.controller.spec.ts
@@ -1,8 +1,9 @@
-import OfficesController from '@/offices/offices.controller';
-import OfficesService from '@/offices/offices.service';
 import { Test, TestingModule } from '@nestjs/testing';
+
 import PrismaModule from '@pr-prisma/prisma.module';
 import PrismaService from '@pr-prisma/prisma.service';
+import OfficesController from '@/offices/offices.controller';
+import OfficesService from '@/offices/offices.service';
 
 describe('OfficesController', () => {
   let controller: OfficesController;
