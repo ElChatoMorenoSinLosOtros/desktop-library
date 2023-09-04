7afe23abc30e306fcb4e77dbe839d348960d46af
Josue_prado <prado69508698@gmial.com>
2023-09-04 12:22:10 -0400

    refactor: update loan item fields

--- apps/client/src/app/pages/loan-management/components/fields/LoanFields.tsx
+++ apps/client/src/app/pages/loan-management/components/fields/LoanFields.tsx
@@ -1,12 +1,13 @@
 function LoanFields() {
   return (
     <div
-      className='p-5 grid place-items-center grid-cols-9 text-lg font-bold font-roboto-mono
+      className='p-5 grid place-items-center grid-cols-10 text-lg font-bold font-roboto-mono
     text-[#1B263B]/60 w-full gap-4'
     >
       <div className='w-full'>ID</div>
-      <div className='w-full'>Material Id</div>
-      <div className='w-full'>Borrower Id</div>
+      <div className='w-full'>Material Type</div>
+      <div className='w-full'>Material Title</div>
+      <div className='w-full'>Borrower</div>
       <div className='w-full'>Loan Date</div>
       <div className='w-full'>Return Date</div>
       <div className='w-full'>Status</div>
--- apps/client/src/app/pages/loan-management/components/list/components/LoanItem.tsx
+++ apps/client/src/app/pages/loan-management/components/list/components/LoanItem.tsx
@@ -4,18 +4,38 @@ import EditIcon from '@mui/icons-material/Edit';
 import InfoIcon from '@mui/icons-material/Info';
 import { useNavigate } from 'react-router-dom';
 import LoanActionButton from './components/LoanActionButton';
+import { useEffect, useState } from 'react';
 
 function LoanItem({ loan }: { loan: Loan }) {
   const navigate = useNavigate();
-  const { removeLoanById } = LibraryAPIService();
+  const { removeLoanById, getMaterialById, getUserById } = LibraryAPIService();
+  const [material, setMaterial] = useState<Material>({} as Material);
+  const [client, setClient] = useState<Client>({} as Client);
+
+  useEffect(() => {
+    getMaterialById({id: loan.materialId})
+      .then(res => setMaterial(res))
+      .catch((error: Error) => {
+        throw new Error(error.message);
+      });
+  }, [loan]);
+
+  useEffect(() => {
+    getUserById({id: String(loan.clientId)})
+      .then(res => setClient(res))
+      .catch((error: Error) => {
+        throw new Error(error.message);
+      });
+  }, [loan]);
 
   return (
-    <div className='grid grid-cols-9 w-full px-6 gap-4'>
+    <div className='grid grid-cols-10 w-full px-6 gap-4'>
       <div>{loan.loanId}</div>
-      <div>{loan.materialId}</div>
-      <div>{loan.clientId}</div>
-      <div>{loan.loanDate.toString()}</div>
-      <div>{loan.returnDate.toString()}</div>
+      <div>{material.type_material}</div>
+      <div>{material.title}</div>
+      <div>{client.name}</div>
+      <div>{loan.loanDate.toString().substring(0, 10)}</div>
+      <div>{loan.returnDate.toString().substring(0, 10)}</div>
       <div>{loan.returned ? 'True' : 'False'}</div>
       <LoanActionButton
         onClick={() => {
