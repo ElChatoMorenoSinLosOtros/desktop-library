1a8725bbc06ca76243ab78161eec41498c8269a4
Severaldog <ignaciovillarroel50@gmail.com>
2023-09-10 17:50:13 -0400

    fix: heap class

--- apps/client/test/heap/heap.test.ts
+++ apps/client/test/heap/heap.test.ts
@@ -1,49 +1,27 @@
 import Heap from '../../trees/heap/heap';
 
 describe('Heap', () => {
-  describe('insert', () => {
-    it('should insert items into the heap correctly', () => {
-      const minHeap = new Heap<number>();
-      minHeap.insert(5);
-      minHeap.insert(3);
-      minHeap.insert(8);
+  test('minHeap', () => {
+    const minHeap = new Heap<number>((a, b) => b - a);
 
-      expect(minHeap.peek()).toBe(3);
-      expect(minHeap.size()).toBe(3);
-    });
+    minHeap.insert(3);
+    minHeap.insert(2);
+    minHeap.insert(1);
+
+    expect(minHeap.extractMax()).toBe(1);
+    expect(minHeap.extractMax()).toBe(2);
+    expect(minHeap.extractMax()).toBe(3);
   });
 
-  describe('extract', () => {
-    it('should extract the minimum item from a min-heap', () => {
-      const minHeap = new Heap<number>();
-      minHeap.insert(5);
-      minHeap.insert(3);
-      minHeap.insert(8);
+  test('maxHeap', () => {
+    const maxHeap = new Heap<number>((a, b) => a - b);
 
-      expect(minHeap.extract()).toBe(3);
-      expect(minHeap.size()).toBe(2);
-    });
+    maxHeap.insert(1);
+    maxHeap.insert(2);
+    maxHeap.insert(3);
 
-    it('should return undefined when extracting from an empty heap', () => {
-      const minHeap = new Heap<number>();
-      expect(minHeap.extract()).toBeUndefined();
-    });
-  });
-
-  describe('peek', () => {
-    it('should return the minimum item without removing it', () => {
-      const minHeap = new Heap<number>();
-      minHeap.insert(5);
-      minHeap.insert(3);
-      minHeap.insert(8);
-
-      expect(minHeap.peek()).toBe(3);
-      expect(minHeap.size()).toBe(3);
-    });
-
-    it('should return undefined when peeking an empty heap', () => {
-      const minHeap = new Heap<number>();
-      expect(minHeap.peek()).toBeUndefined();
-    });
+    expect(maxHeap.extractMax()).toBe(3);
+    expect(maxHeap.extractMax()).toBe(2);
+    expect(maxHeap.extractMax()).toBe(1);
   });
 });
--- apps/client/trees/heap/heap.ts
+++ apps/client/trees/heap/heap.ts
@@ -1,17 +1,10 @@
 export default class Heap<T> {
   private data: T[] = [];
 
-  private isMinHeap: boolean;
+  private compare: (a: T, b: T) => number;
 
-  constructor(isMinHeap = true) {
-    this.isMinHeap = isMinHeap;
-  }
-
-  private compare(a: T, b: T): boolean {
-    if (this.isMinHeap) {
-      return a < b;
-    }
-    return a > b;
+  constructor(compare: (a: T, b: T) => number) {
+    this.compare = compare;
   }
 
   private swap(i: number, j: number): void {
@@ -21,25 +14,25 @@ export default class Heap<T> {
   private heapify(index: number): void {
     const left = 2 * index + 1;
     const right = 2 * index + 2;
-    let smallestOrLargest = index;
+    let largest = index;
 
     if (
       left < this.data.length &&
-      this.compare(this.data[left], this.data[smallestOrLargest])
+      this.compare(this.data[left], this.data[largest]) > 0
     ) {
-      smallestOrLargest = left;
+      largest = left;
     }
 
     if (
       right < this.data.length &&
-      this.compare(this.data[right], this.data[smallestOrLargest])
+      this.compare(this.data[right], this.data[largest]) > 0
     ) {
-      smallestOrLargest = right;
+      largest = right;
     }
 
-    if (smallestOrLargest !== index) {
-      this.swap(index, smallestOrLargest);
-      this.heapify(smallestOrLargest);
+    if (largest !== index) {
+      this.swap(index, largest);
+      this.heapify(largest);
     }
   }
 
@@ -49,37 +42,64 @@ export default class Heap<T> {
     let currentIndex = this.data.length - 1;
     while (currentIndex > 0) {
       const parentIndex = Math.floor((currentIndex - 1) / 2);
-      if (this.compare(this.data[currentIndex], this.data[parentIndex])) {
-        this.swap(currentIndex, parentIndex);
-        currentIndex = parentIndex;
-      } else {
+      if (this.compare(this.data[currentIndex], this.data[parentIndex]) <= 0) {
         break;
       }
+      this.swap(currentIndex, parentIndex);
+      currentIndex = parentIndex;
     }
   }
 
-  public extract(): T | undefined {
+  public searchByProperty(property: keyof T, value: any): T[] {
+    const results: T[] = [];
+    this.data.forEach(item => {
+      if (item[property] === value) {
+        results.push(item);
+      }
+    });
+    return results;
+  }
+
+  public extractMax(): T | undefined {
     if (this.data.length === 0) {
       return undefined;
     }
-
     if (this.data.length === 1) {
       return this.data.pop();
     }
-
     const root = this.data[0];
-    const lastItem = this.data.pop();
-
-    if (lastItem !== undefined) {
-      this.data[0] = lastItem;
-      this.heapify(0);
-    }
-
+    this.data[0] = this.data.pop()!;
+    this.heapify(0);
     return root;
   }
 
-  public peek(): T | undefined {
+  public getRoot(): T | undefined {
+    if (this.data.length === 0) {
+      return undefined;
+    }
     return this.data[0];
+  }
+
+  public getMax(): T | undefined {
+    if (this.data.length === 0) {
+      return undefined;
+    }
+    return this.data.reduce((max, current) =>
+      this.compare(max, current) >= 0 ? max : current
+    );
+  }
+
+  public getMin(): T | undefined {
+    if (this.data.length === 0) {
+      return undefined;
+    }
+    return this.data.reduce((min, current) =>
+      this.compare(min, current) <= 0 ? min : current
+    );
+  }
+
+  public isEmpty(): boolean {
+    return this.data.length === 0;
   }
 
   public size(): number {
