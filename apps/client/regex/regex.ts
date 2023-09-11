e9e2ba1dc26773bff6eaf6df8191b6ea6bbfab52
Hugo Oropeza <oropezahugo35@gmail.com>
2023-09-10 21:54:22 -0400

    feat: add regex for phone, isbn, positive numbers

---
+++ apps/client/regex/regex.ts
@@ -0,0 +1,35 @@
+function isGreaterThanZero(numberToCheck: number): Promise<boolean> {
+  return new Promise((resolve, reject) => {
+    if (numberToCheck > 0) {
+      resolve(true);
+    }
+    else {
+      reject(new Error('number is negative'))
+    }
+  })
+}
+
+
+function validateISBN(textToCheck: string): Promise<boolean> {
+  const regex = /^\d{3}-\d{10}$/;
+  return new Promise((resolve, reject) => {
+    if (regex.test(textToCheck)) {
+      resolve(true)
+    } else {
+      reject(new Error('invalid ISBN'))
+    }
+  })
+}
+
+function validatePhoneNumber(textToCheck: string): Promise<boolean> {
+  const regex = /^\d{8}$/;
+  return new Promise((resolve, reject) => {
+    if (regex.test(textToCheck)) {
+      resolve(true)
+    } else {
+      reject(new Error('invalid phone'))
+    }
+  })
+}
+
+export default { isGreaterThanZero, validateISBN, validatePhoneNumber };
